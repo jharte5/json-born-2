75ad5899afa46c442c06efcfedbe7b127d58ae59
NikolayK <nikolay.kim@codeimmersive.com>
2019-12-16 15:04:33 -0500

    solves GET users command

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
 const fs = require('fs');
 
-const ind = process.argv[2];
+const funcName = process.argv[2];
+const usersARGV = process.argv[3];
 const userName = process.argv
 
-fs.readFile('./users.json', function(error, data) {
-    const users = JSON.parse(data)
-    console.log(users)
-})
+
+if(funcName === 'GET' && usersARGV === 'users'){
+    fs.readFile('./users.json', function(error, data) {
+        const users = JSON.parse(data)
+        console.log(users)
+    })
+}
