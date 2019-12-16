a7fb01f3f818bca4d1c7a7dc435a0818f8ece3c0
NikolayK <nikolay.kim@codeimmersive.com>
2019-12-16 15:48:38 -0500

    adds post function

--- main.js
+++ main.js
@@ -19,3 +19,25 @@ if(funcName === 'GET'){
         }
     })
 } 
+
+if(funcName === 'POST'){
+    fs.readFile('./users.json', function(error, data) {
+        const users = JSON.parse(data);
+        console.log(users)
+        const addedUser = {
+            "index": users.length,
+            "age": process.argv[5],
+            "eyeColor": process.argv[6],
+            "name": process.argv[4],
+            "friends": []
+        }
+        if (usersARGV === 'user') {
+            users.push(addedUser)
+            const newUserList = JSON.stringify(users)
+            fs.writeFileSync('users.json', newUserList)
+            console.log('New user added')
+        } else if (usersARGV === 'friends') {
+            console.log(users[index])
+        }
+    })
+}
