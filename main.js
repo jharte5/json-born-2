97e36ff81015470ab9739f805030572a252e0800
Sung Paik <sung.paik@codeimmersives.com>
2019-12-17 11:25:44 -0500

    completed exercise and changed writefilesync to write file

--- main.js
+++ main.js
@@ -2,28 +2,26 @@ const fs = require('fs');
 
 const funcName = process.argv[2];
 const usersARGV = process.argv[3];
-const userName = process.argv
 const index = process.argv[4]
 
 
-if(funcName === 'GET'){
+if (funcName === 'GET') {
     fs.readFile('./users.json', function(error, data) {
         const users = JSON.parse(data);
 
         if (usersARGV === 'users') {
-            console.log(users)
+            console.log(users);
         } else if (usersARGV === 'user') {
-            console.log(users[index])
+            console.log(users[index]);
         } else if (usersARGV === 'friends') {
-            console.log(users[index].friends)
+            console.log(users[index].friends);
         }
     })
 } 
 
-if(funcName === 'POST'){
+if (funcName === 'POST') {
     fs.readFile('./users.json', function(error, data) {
         const users = JSON.parse(data);
-        console.log(users)
         const addedUser = {
             "index": users.length,
             "age": process.argv[5],
@@ -31,13 +29,52 @@ if(funcName === 'POST'){
             "name": process.argv[4],
             "friends": []
         }
-        if (usersARGV === 'user') {
-            users.push(addedUser)
-            const newUserList = JSON.stringify(users)
-            fs.writeFileSync('users.json', newUserList)
-            console.log('New user added')
-        } else if (usersARGV === 'friends') {
-            console.log(users[index])
+
+        const newFriend = {
+            "id": this.length,
+            "name": process.argv[5]
         }
+
+        if (usersARGV === 'user') {
+            users.push(addedUser);
+            const newUserList = JSON.stringify(users, null, 2);
+
+            fs.writeFile('users.json', newUserList, function(err) {
+                if(err) throw err;
+            })
+        } else if (usersARGV === 'friends') {
+            users[index].friends.push(newFriend);
+            const newFriendList = JSON.stringify(users, null, 2);
+
+            fs.writeFile('users.json', newFriendList, function(err) {
+                if(err) throw err;
+            })
+        }
+    })
+}
+
+if (funcName === 'PUT') {
+    fs.readFile('./users.json', function(error, data) {
+        const users = JSON.parse(data);
+        const newValue = process.argv[6];
+
+        users[index][process.argv[5]] = newValue;
+        const updateUsers = JSON.stringify(users, null, 2)
+
+        fs.writeFile('users.json', updateUsers, function(err) {
+            if(err) throw err;
+        })
+    })
+}
+
+if (funcName === 'DELETE') {
+    fs.readFile('./users.json', function(error, data) {
+        const users = JSON.parse(data);
+        users.splice(index, 1);
+        const deletedUser = JSON.stringify(users, null, 2)
+
+        fs.writeFile('users.json', deletedUser, function(err) {
+            if(err) throw err;
+        })
     })
 }
